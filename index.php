0c9e04aecda96d789325700e547a8f5c6635af9b
SatayuPrommarat <108518654+SatayuPrommarat@users.noreply.github.com>
2022-10-21 16:53:31 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Resume</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+</style>
+</head>
+<body class="w3-light-grey">
+
+<!-- Page Container -->
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+  
+    <!-- Left Column -->
+    <div class="w3-third">
+    
+      <div class="w3-white w3-text-grey w3-card-4">
+        <div class="w3-display-container">
+          <img src="https://scontent.fbkk10-1.fna.fbcdn.net/v/t1.6435-9/167084875_1806199052887232_7517792583889755244_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeFt-h2uB8hCrW0TWURbrqpr0Qh9YRpIDYfRCH1hGkgNh2oyQ-XWU4-94Lwxz8b_s_PuFXAO-7egdfs3FFyQ1nK-&_nc_ohc=tPzyhgKRbNEAX_o01do&_nc_ht=scontent.fbkk10-1.fna&oh=00_AT8JSPVdGBCPXMe8d1c2ZjfLhfHdzF9ZLQoD-g0oOOGfFQ&oe=6311983F" style="width:100%" alt="Avatar">
+          <div class="w3-display-bottomleft w3-container w3-text-black">
+           
+          </div>
+        </div>
+        <div class="w3-container">
+          <p><i class="fa fa-person fa-fw w3-margin-right w3-large w3-text-blue"></i><b>Satayu Prommarat</b></p>
+          <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-blue"></i>Undergraduate student</p>
+          <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-blue"></i>Maha Sarakham University, Thailand</p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-blue"></i>64010974011@msu.ac.th</p>
+          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-blue"></i>093-374-0414</p>
+          <hr>
+
+          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-red"></i>Skills</b></p>
+          <p>Python</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-blue" style="width:60%">60%</div>
+          </div>
+          <p>C#</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-blue" style="width:60%">
+              <div class="w3-center w3-text-white">60%</div>
+            </div>
+          </div>
+          <p>Microsoft</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-blue" style="width:70%">70%</div>
+          </div>
+          <p>Data analytics</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-blue" style="width:60%">60%</div>
+          </div>
+          <br>
+
+          <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-red"></i>Languages</b></p>
+                     <p>Thai</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-center w3-blue" style="height:24px;width:90%">90%</div>
+          </div>
+          <p>English</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-center w3-blue" style="height:24px;width:50%">50%</div>
+          </div>
+          <p>Lao</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-center w3-blue" style="height:24px;width:70%">70%</div>
+          </div>
+          <br>
+        </div>
+      </div><br>
+
+    <!-- End Left Column -->
+    </div>
+
+    <!-- Right Column -->
+    <div class="w3-twothird">
+    
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-red"></i>Work Experience</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Apprentice / Thailand post co.,ltd.,Roi-et, Thailand</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>March 2020 - May 2020</h6>
+        </div>    
+      </div>
+        
+               
+      <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-red"></i>Education</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>High School</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2014 - 2019</h6>
+          <p>Phanomphai wittayakarn School,Roi-et</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Diploma/High Vocational Certificate</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2019 - 2021</h6>
+          <p>Yasothon technical college,Yasothon</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Bachelor's Degree </b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2021 - 2023</h6>
+          <p>Mahasarakham University,Maha sarakham</p>
+          </p>
+        </div>
+      </div>
+      
+       <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-arrow-up fa-fw w3-margin-right w3-xxlarge w3-text-red"></i>Self-Development</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Certification: Google Data Studio </b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2022</h6>      
+          <hr>
+        </div>
+        
+         <div class="w3-container">
+          <h5 class="w3-opacity"><b> Course : Routing & Switching Course  </b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2021</h6>      
+          <hr>
+        </div>
+        
+         <div class="w3-container">
+          <h5 class="w3-opacity"><b> Cisco Networking Academy Program </b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2021</h6>       
+          <hr>
+        </div>
+        
+         <div class="w3-container">
+          <h5 class="w3-opacity"><b> Technology And Innovation Skill AR VR MR and 3D Development Program </b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2021</h6>  
+          <hr>
+        </div>
+          <div class="w3-container">
+          <h5 class="w3-opacity"><b> Courese: Applications of TCP/IP Network </b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2021</h6>  
+          <hr>
+        </div>
+
+      </div>
+
+    <!-- End Right Column -->
+    </div>
+    
+  <!-- End Grid -->
+  </div>
+  
+  <!-- End Page Container -->
+</div>
+
+<footer class="w3-container w3-blue w3-center w3-margin-top">
+  <p>Find me on social media.</p>
+  <i class="fa fa-facebook-official w3-hover-opacity"></i>
+  <i class="fa fa-instagram w3-hover-opacity"></i>
+  <i class="fa fa-snapchat w3-hover-opacity"></i>
+  <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+  <i class="fa fa-twitter w3-hover-opacity"></i>
+  <i class="fa fa-linkedin w3-hover-opacity"></i>
+  <p>Powered by <a href="http://mbs.msu.ac.th/2022/" target="_blank">MBS,MSU</a></p>
+</footer>
+
+</body>
+</html>
